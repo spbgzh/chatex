d0f3f8bfff79110bc624ba39726c9d947df3987f
GG <72414103+spbgzh@users.noreply.github.com>
2021-11-07 12:54:52 +0300

    Revert "Sign-up And ResetPassword Function"

--- src/main/java/com/chatex/service/UserRoleService.java
+++
@@ -1,8 +0,0 @@
-package com.chatex.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserRoleService {
-
-}
--- src/test/java/com/chatex/ChatexApplicationTests.java
+++ src/test/java/com/chatex/ChatexApplicationTests.java
@@ -3,14 +3,11 @@ package com.chatex;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
 @SpringBootTest
 class ChatexApplicationTests {
 
     @Test
-    void test(){
-
-
+    void contextLoads() {
     }
 
 }
